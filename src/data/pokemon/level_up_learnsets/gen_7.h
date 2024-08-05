b770d98ec8c784bf835ceab1e60c45114e55a8cc
Kasen <forerunnerin@hotmail.com>
2024-08-05 16:50:06 +0300

    fix learnsets

--- src/data/pokemon/level_up_learnsets/gen_7.h
+++ src/data/pokemon/level_up_learnsets/gen_7.h
@@ -7113,8 +7113,6 @@ static const struct LevelUpMove sCombuskenLevelUpLearnset[] = {
 static const struct LevelUpMove sBlazikenLevelUpLearnset[] = {
     LEVEL_UP_MOVE( 0, MOVE_BLAZE_KICK),
     LEVEL_UP_MOVE( 1, MOVE_BLAZE_KICK),
-    LEVEL_UP_MOVE( 1, MOVE_ROCK_CLIMB),
-    LEVEL_UP_MOVE( 1, MOVE_SURF),
     LEVEL_UP_MOVE( 1, MOVE_DOUBLE_KICK),
     LEVEL_UP_MOVE( 1, MOVE_FLARE_BLITZ),
     LEVEL_UP_MOVE( 1, MOVE_FIRE_PUNCH),
