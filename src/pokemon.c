0ab471f61a03a4d8da96954143625636ddba8f4c
Kasenn <forerunnerin@gmail.com>
2026-04-30 12:36:20 +0300

    fix rare candies healing too much

--- src/pokemon.c
+++ src/pokemon.c
@@ -3962,7 +3962,8 @@ bool8 PokemonUseItemEffects(struct Pokemon *mon, u16 item, u8 partyIndex, u8 mov
                             dataUnsigned = currentHP + dataUnsigned;
                             if (dataUnsigned > maxHP)
                                 dataUnsigned = maxHP;
-                            SetMonData(mon, MON_DATA_HP, &dataUnsigned);
+                            if (modifyStats)
+                                SetMonData(mon, MON_DATA_HP, &dataUnsigned);
                             retVal = FALSE;
                         }
                         effectFlags &= ~(ITEM4_REVIVE >> 2);
